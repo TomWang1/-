58540736632a8a21c7f1305f19ac3c3a7ef340a6
杨胜超 <xiaosheng0601@qq.com>
2016-01-05 14:16:05 +0800

    Initial Commit

---
+++ HomeHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeHome
+//
+//  Created by Victor on 16/1/5.
+//  Copyright © 2016年 Victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HomeHome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeHome
+//
+//  Created by Victor on 16/1/5.
+//  Copyright © 2016年 Victor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
